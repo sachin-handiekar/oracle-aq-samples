cf441f8e05408ea6d25fdbcc39cd0495ff922f12
Sachin Handiekar <sach21@gmail.com>
2011-10-30 19:48:42 +0000

    initial Commit

---
+++ sql/Enqueue-XMLType-Message.sql
@@ -0,0 +1,20 @@
+DECLARE 
+ 
+  queue_options DBMS_AQ.ENQUEUE_OPTIONS_T; 
+  message_properties DBMS_AQ.MESSAGE_PROPERTIES_T; 
+  message_id RAW(16); 
+  message SYS.XMLType; 
+ 
+BEGIN 
+ 
+  message := sys.XMLType.createXML('<sample>hello world</sample>');
+  DBMS_AQ.ENQUEUE( queue_name => 'q_history', 
+                   enqueue_options => queue_options, 
+                   message_properties => message_properties, 
+                   payload => message, 
+                   msgid => message_id); 
+ 
+  COMMIT;
+ 
+END;
+ 
